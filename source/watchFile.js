b6e5f53d0647f272279d6fa960321de806ad009f
Safi <sfmissive@gmail.com>
2019-03-05 23:00:21 +0200

    ➕ Add flag determining logging messages behavior.

--- source/watchFile.js
+++ source/watchFile.js
@@ -7,7 +7,8 @@ export async function watchFile({
     // Following delay solves the issue of closley received notifications, preventing duplicate actions for the same related notifications (e.g. docker-windows-volume-watcher mistakengly triggers duplicate notifications for each chagne).
     notificationTriggerDelay = 100, // 100 ms between accepting notifications and triggering action. This prevents duplicate executions fron wrong immediate duplicate notification fo the container.
     notificationTrigger = true, // on / off switch for taking action after a notification received
-    ignoreNodeModules = true
+    ignoreNodeModules = true, 
+    logMessage = false // console log messages on events e.g. log list of files being watched.
 }) {
     let watcher = chokidar.watch(fileArray, {
         ignored: (ignoreNodeModules) ? new RegExp(/node_modules/) : false,
@@ -17,9 +18,11 @@ export async function watchFile({
     return new Promise((resolve, reject) => {
         watcher
             .on('ready', path => {
-                console.group('• Watching the following list of paths:')
-                console.log(watcher.getWatched()) // list watched filse
-                console.groupEnd()
+                if(logMessage) {
+                    console.group('• Watching the following list of paths:')
+                    console.log(watcher.getWatched()) // list watched filse
+                    console.groupEnd()
+                }
                 resolve()
             })
             .on('add', path => { // when target/path added to watcher
@@ -27,13 +30,13 @@ export async function watchFile({
             })
             .on('change', path => {
                 if(notificationTrigger) {
-                    console.log(`• File ${path} has been changed.`)
+                    if(logMessage) console.log(`• File ${path} has been changed.`)
                     triggerCallback()
                 }
                 // allow triggering of action from next notification only after delay.
                 notificationTrigger = false 
                 setTimeout(() => { notificationTrigger = true }, notificationTriggerDelay);
             })
-            .on('unlink', path => console.log(`• File ${path} has been removed. No action taken regarding running tests.`))
+            .on('unlink', path => { if(logMessage) console.log(`• File ${path} has been removed. No action taken regarding running tests.`) })
     })
 }
