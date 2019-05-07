c801774f192a33fd4a052a536f78fb25976090a4
Dr. Safi <sfmissive@gmail.com>
2019-05-07 03:45:44 +0300

    build

--- source/script.js
+++ source/script.js
@@ -1,7 +1,21 @@
-import { watchFile } from './watchFile.js'
-import { runWebappServer } from './webappServer.js'
+"use strict";
 
-export { 
-    watchFile,
-    runWebappServer
-}
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "watchFile", {
+  enumerable: true,
+  get: function () {
+    return _watchFile.watchFile;
+  }
+});
+Object.defineProperty(exports, "runWebappServer", {
+  enumerable: true,
+  get: function () {
+    return _webappServer.runWebappServer;
+  }
+});
+
+var _watchFile = require("./watchFile.js");
+
+var _webappServer = require("./webappServer.js");
