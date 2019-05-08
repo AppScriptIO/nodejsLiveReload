8f6f4ac4410d1c37ab75c9435b9e2658db9c90d4
Dr. Safi <sfmissive@gmail.com>
2019-05-08 05:45:46 +0300

    build

--- source/webappServer.js
+++ source/webappServer.js
@@ -1,45 +1,55 @@
-import path from 'path'
-const BrowserSync = require('browser-sync')
-import config from '../configuration/configuration.js'
-const ServerLivereload = require('./utility/processReload.js')
+"use strict";
 
-export function runWebappServer({
-    filename = 'entrypoint.js',
-    fileBasePath = 'path' || config.directory.serverSidePath, // /project/application/distribution/serverSide/
-    targetAppHost = 'localhost',
-    targetAppPort = 9903,
-    browserSync, // BrowserSync instance
-    serverLivereload,
-    // File list to watch - Uses globs array for defining files patterns - https://github.com/isaacs/node-glob
-    watchFileList_clientSide,
-    watchFileList_serverSide,
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.runWebappServer = runWebappServer;
+
+var _configuration = _interopRequireDefault(require("../configuration/configuration.js"));
+
+const BrowserSync = require('browser-sync');
+
+const ServerLivereload = require('./utility/processReload.js');
+
+function runWebappServer({
+  filename = 'entrypoint.js',
+  fileBasePath = 'path' || _configuration.default.directory.serverSidePath,
+  // /project/application/distribution/serverSide/
+  targetAppHost = 'localhost',
+  targetAppPort = 9903,
+  browserSync,
+  // BrowserSync instance
+  serverLivereload,
+  // File list to watch - Uses globs array for defining files patterns - https://github.com/isaacs/node-glob
+  watchFileList_clientSide,
+  watchFileList_serverSide
 } = {}) {
-    console.info(`☕ Running script "${filePath}${filename}". With arguments: debugArguments.join()`)
+  console.info(`☕ Running script "${filePath}${filename}". With arguments: debugArguments.join()`);
+  browserSync = BrowserSync.create('Info - locahost server');
+  browserSync.init({
+    host: targetAppHost,
+    port: targetAppPort,
+    proxy: {
+      target: targetAppHost // ws: true // when localhost webapp uses websocket also.
 
-    browserSync = BrowserSync.create('Info - locahost server')
-    browserSync.init({
-        host: targetAppHost,
-        port: targetAppPort,
-        proxy: {
-            target: targetAppHost,
-            // ws: true // when localhost webapp uses websocket also.
-        },
-        ui: {
-            port: 9901,
-            weinre: { // remove debugger
-                port: 9902
-            }
-        },
-        // logLevel: 'debug',
-        logConnections: true,
-        open: false, // open browser false.
-        scriptPath: () => `http://${targetAppHost}/browser-sync/browser-sync-client.js`
-    })
-
-    // serverLivereload = new ServerLivereload(debugArguments, { filename, filePath })
-    // serverLivereload.on('reload', () => {
-    //     browserSync.reload()
-    // })
-
-    // serverLivereload.reload()
+    },
+    ui: {
+      port: 9901,
+      weinre: {
+        // remove debugger
+        port: 9902
+      }
+    },
+    // logLevel: 'debug',
+    logConnections: true,
+    open: false,
+    // open browser false.
+    scriptPath: () => `http://${targetAppHost}/browser-sync/browser-sync-client.js`
+  }); // serverLivereload = new ServerLivereload(debugArguments, { filename, filePath })
+  // serverLivereload.on('reload', () => {
+  //     browserSync.reload()
+  // })
+  // serverLivereload.reload()
 }
